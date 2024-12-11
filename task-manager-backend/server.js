14f0193653a1b21f7725567372460ee93116e086
VighneshVejandla2003 <vighnesh.2003vejandla@gmail.com>
2024-12-11 08:51:41 +0530

    changes in server.js

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
